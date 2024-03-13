46a71d437d755c8631eda490870f9b64d0b03ed0
VFALAN <alanvillafan95@gmail.com>
2024-03-13 09:13:22 -0600

    first commit

---
+++ src/main/java/com/msc/ms/users/MscMsUsersApplication.java
@@ -0,0 +1,13 @@
+package com.msc.ms.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscMsUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscMsUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msc/ms/users/MscMsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msc.ms.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscMsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
