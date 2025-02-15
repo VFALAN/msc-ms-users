cdca46d3825227b13748355eb79718ca999caba5
VFALAN <alanvillafan95@gmail.com>
2025-02-15 07:14:28 -0600

    correction in spelled fields

--- src/main/java/com/msc/ms/users/user/model/response/UserRegistryResponse.java
+++ src/main/java/com/msc/ms/users/user/model/response/UserRegistryResponse.java
@@ -8,6 +8,6 @@ import lombok.*;
 @NoArgsConstructor
 public class UserRegistryResponse {
     private Integer idUser;
-    private String username;
+    private String userName;
     private String email;
 }
